c4a8c6ef375c39fd010389eaf639bfef95848a88
Jay Salvat <jay.salvat@gmail.com>
2011-03-24 10:57:27 +0100

    Added metakey support for Mac users (michal-krause)

--- markitup/jquery.markitup.js
+++ markitup/jquery.markitup.js
@@ -520,7 +520,7 @@
 			function keyPressed(e) { 
 				shiftKey = e.shiftKey;
 				altKey = e.altKey;
-				ctrlKey = (!(e.altKey && e.ctrlKey)) ? e.ctrlKey : false;
+				ctrlKey = (!(e.altKey && e.ctrlKey)) ? e.ctrlKey || e.metaKey : false;
 
 				if (e.type === 'keydown') {
 					if (ctrlKey === true) {
