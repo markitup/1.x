5034579f24062baf385d43687e44b497cc72c2b8
Jay Salvat <jay@jaysalvat.com>
2013-07-12 12:43:52 +0200

    Code formating

--- markitup/jquery.markitup.js
+++ markitup/jquery.markitup.js
@@ -636,9 +636,12 @@
 			function remove() {
 				$$.unbind(".markItUp").removeClass('markItUpEditor');
 				$$.parent('div').parent('div.markItUp').parent('div').replaceWith($$);
-				var relative_ref = $$.parent('div').parent('div.markItUp').parent('div');
-				if (relative_ref.length)
-				    relative_ref.replaceWith($$);
+
+				var relativeRef = $$.parent('div').parent('div.markItUp').parent('div');
+				if (relativeRef.length) {
+				    relativeRef.replaceWith($$);
+				}
+				
 				$$.data('markItUp', null);
 			}
 
