61a54f58daad3fe5c1fe39100ec2f2c2300e1065
Vitaliy Ralle <email4vit@gmail.com>
2013-09-26 15:33:09 +0300

    Adding More Customization
    Add Custom tags to button name. Backward Compatibility.
    markupSet: [
    	{name:'<i class="icon-bold"></i>', title:"Font Bold", key:'B', openWith:'[b]', closeWith:'[/b]', className: 'bb-bold'},
    	{name:'Tont Italic', key:'I', openWith:'[i]', closeWith:'[/i]', className: 'bb-bold'}
    }
    Example: http://test.hw-lab.com/viewtopic.php?f=3&t=8&start=40

--- markitup/jquery.markitup.js
+++ markitup/jquery.markitup.js
@@ -202,7 +202,7 @@
 				$('li:hover > ul', ul).css('display', 'block');
 				$.each(markupSet, function() {
 					var button = this, t = '', title, li, j;
-					title = (button.key) ? (button.name||'')+' [Ctrl+'+button.key+']' : (button.name||'');
+					button.title ? title = (button.key) ? (button.title||'')+' [Ctrl+'+button.key+']' : (button.title||'') : title = (button.key) ? (button.name||'')+' [Ctrl+'+button.key+']' : (button.name||'');
 					key   = (button.key) ? 'accesskey="'+button.key+'"' : '';
 					if (button.separator) {
 						li = $('<li class="markItUpSeparator">'+(button.separator||'')+'</li>').appendTo(ul);
